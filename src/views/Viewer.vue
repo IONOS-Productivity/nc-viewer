9ca1c15953ea85b3cbb8ebb5e9a161e09d4e4755
Franziska Bath <franziska.bath@strato.de>
2024-09-02 13:40:22 +0200

    IONOS: feat(Viewer): hide file name in modal header when mime image shown
    Signed-off-by: Franziska Bath <franziska.bath@strato.de>

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -397,7 +397,11 @@ export default defineComponent({
 		},
 
 		modalTitle() {
-			return this.currentFile.basename
+			if (!configModule.alwaysShowViewer) {
+				return this.currentFile.basename
+			}
+
+			return this.currentFile?.modal?.name === 'Default' ? '' : this.currentFile.basename
 		},
 
 		showComparison() {
