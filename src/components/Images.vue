81932dba80d7b6e56f1ef98a54d2379b70c39330
Thomas Lehmann <t.lehmann@strato.de>
2024-09-12 15:41:35 +0200

    IONOS: fix(Images): hide loading spinner on failed image load
    == The cause
    
    Previously the code attempted to load a preview of an image.
    
    If loading this preview image failed it was attempted to load the
    original image.
    
    Load errors of images were only handled _once_. This meant that a
    load error for the original image was never handled, thus the
    viewer was still in loading state and showed a browser-dependant
    "broken image" replacement icon.
    
    == The fix
    
    Now further image load errors are handled too. In case the original
    fails too, the loading state is ended and a placeholder text is shown.
    
    The default preview component, which was introduced to show something
    for any mimetype if configured, is now also used as a fallback.
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -12,7 +12,8 @@
 			@close="onClose" />
 
 		<template v-else-if="data !== null">
-			<img v-if="!livePhotoCanBePlayed"
+			<Default v-if="originalFailed" :basename="basename" :mime="mime" />
+			<img v-else-if="!livePhotoCanBePlayed"
 				ref="image"
 				:alt="alt"
 				:class="{
@@ -22,7 +23,7 @@
 				}"
 				:src="data"
 				:style="imgStyle"
-				@error.capture.prevent.stop.once="onFail"
+				@error.capture.prevent.stop="onFail"
 				@load="updateImgSize"
 				@wheel.stop.prevent="updateZoom"
 				@dblclick.prevent="onDblclick"
@@ -82,6 +83,7 @@ import { basename } from '@nextcloud/paths'
 import { translate } from '@nextcloud/l10n'
 import { NcLoadingIcon } from '@nextcloud/vue'
 
+import Default from './Default.vue'
 import ImageEditor from './ImageEditor.vue'
 import { findLivePhotoPeerFromFileId } from '../utils/livePhotoUtils'
 import { getDavPath } from '../utils/fileUtils'
@@ -93,6 +95,7 @@ export default {
 	name: 'Images',
 
 	components: {
+		Default,
 		ImageEditor,
 		PlayCircleOutline,
 		NcLoadingIcon,
@@ -111,6 +114,7 @@ export default {
 			shiftY: 0,
 			zoomRatio: 1,
 			previewFailed: false,
+			originalFailed: false,
 			livePhotoCanBePlayed: false,
 			zooming: false,
 			pinchDistance: 0,
@@ -420,9 +424,17 @@ export default {
 
 		// Fallback to the original image if not already done
 		onFail() {
+			if (this.originalFailed) {
+				// Loading the original image was already attempted, don't bother handling more errors
+				return
+			}
 			if (!this.previewFailed) {
-				console.error(`Loading of file preview ${basename(this.src)} failed, falling back to original file`)
 				this.previewFailed = true
+				console.error(`Loading of file preview ${basename(this.src)} failed, falling back to original file`)
+			} else {
+				this.originalFailed = true
+				console.error(`Loading of the original image ${basename(this.source)} failed too`)
+				this.doneLoading()
 			}
 		},
 		doneLoadingLivePhoto() {
