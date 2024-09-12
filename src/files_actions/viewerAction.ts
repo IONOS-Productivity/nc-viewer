338269189520554f1be0ae63e1bac9ac7f865af8
Kai Henseler <kai.henseler@strato.de>
2024-09-12 12:19:32 +0200

    fix(Viewer): remove contextmenu view action for folders
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/files_actions/viewerAction.ts
+++ src/files_actions/viewerAction.ts
@@ -79,6 +79,10 @@ export function registerViewerAction() {
 
 			// Always enabled if configured so
 			if (configModule.alwaysShowViewer) {
+				// disable for folders
+				if (nodes.some(node => node.type === 'folder')) {
+					return false
+				}
 				return true
 			}
 
