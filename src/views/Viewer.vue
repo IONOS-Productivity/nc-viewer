49707fe32b5d77c566b994f684296e231f33ce7b
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-06-23 16:48:20 +0200

    IONOS: refactor(Viewer): remove unnecessary whitespace in node validation logic
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -990,9 +990,6 @@ export default defineComponent({
 						if (nodes.some(node => !(node.isDavRessource && node.root?.startsWith('/files')))) {
 							return false
 						}
-
-
-
 						// Faster to check if at least one node doesn't match the requirements
 						return !nodes.some(node => (
 							(node.permissions & Permission.READ) === 0
